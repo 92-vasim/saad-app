e2870dd5743e2792039595a4959010c285513bda
Vasim <mwaseembly92@gmail.com>
2023-03-30 02:54:39 +0530

    database connection hogaya

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 // Importin express module/library
 const express = require('express')
+const mongoose = require('mongoose');
 
 // Initializing application name 
 const app = express()
 
 // Assiging port number 
 const port = 3000
+
+// Mongodb connection uri 
+// getting-started.js
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/saad-notes-app');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
 
 // Middleware assistant for getting data from front end
 app.use(express.urlencoded({ extended: true }))
@@ -19,6 +31,18 @@ app.set('view engine', 'ejs');
 // Middleware 
 app.use(express.static('static'))
 
+// Creating schema for db
+// You can change schema name 
+// Add fields for data
+const notesSchema = new mongoose.Schema({
+  title: String,
+  subject: String
+}); // Schema is ready 
+
+// Creating a model 
+// Write you collection name 
+// Add your collection name and schema name in arguements 
+const Note = mongoose.model('Note', notesSchema);
 
 // Here we are sending response on the browser
 // These are page rendering routes
@@ -32,6 +56,7 @@ app.get('/', (req, res) => { // it is even called "end point, router"
 app.post('/postNote', (req, res) => { // it is even called "end point, router"
   let data = req.body
   console.log(data)
+  Note.create(data)
   res.status(200).json({success:true})
 })
 
